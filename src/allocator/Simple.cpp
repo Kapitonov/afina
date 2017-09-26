aa3354955099d266ed668cb59f11cf6e92b10558
Kapitonov <kapitonov.a.e@yandex.ru>
2017-09-26 10:48:27 +0300

    fix

--- src/allocator/Simple.cpp
+++ src/allocator/Simple.cpp
@@ -4,258 +4,251 @@
 #include <afina/allocator/Pointer.h>
 
 namespace Afina {
-namespace Allocator {
+    namespace Allocator {
 
-Simple::Simple(void *base, size_t size)
-    : _base(base), _base_len(size), _first_empty_block((Block *)_base),
-      _descriptor((void *)((char *)_base + _base_len)), _first_empty_descriptor(nullptr), _number_descriptor(0) {
-    _first_empty_block->size = size;
-    _first_empty_block->next = nullptr;
-}
+        Simple::Simple(void *base, size_t size)
+                : _base(base), _base_len(size), _first_empty_block((Block *)_base),
+                  _descriptor((void *)((char *)_base + _base_len)), _first_empty_descriptor(nullptr), _number_descriptor(0) {
+            _first_empty_block->size = size;
+            _first_empty_block->next = nullptr;
+        }
 
 /**
  * TODO: semantics
  * @param N size_t
  */
-Pointer Simple::alloc(size_t N) {
-    try {
-        if (N < sizeof(void *))
-            N = sizeof(void *); // Не выделяем памяти меньше чем возможно освободить.
-        // Проверка на наличие свободной памяти
+        Pointer Simple::alloc(size_t N) {
 
-        if (!_first_empty_block) {
-            throw AllocError(AllocErrorType::NoMemory, "No Memory");
-        }
-        // Ищем дескриптор для выделяемой памяти
-        if (!_first_empty_descriptor) {
-            Block *temp = _first_empty_block;
-            while (temp->next) {
-                temp = temp->next;
-            }
+            if (N < sizeof(void *))
+                N = sizeof(void *); // Не выделяем памяти меньше чем возможно освободить.
+            // Проверка на наличие свободной памяти
 
-            if ((temp->size >= 2 * (sizeof(size_t) + sizeof(void *))) &&
-                ((void *)((char *)temp + temp->size) == _descriptor)) {
-                temp->size -= sizeof(void *);
-                _first_empty_descriptor = (void *)((char *)temp + temp->size);
-                *((void **)_first_empty_descriptor) = nullptr;
-            } else {
-                defrag();
-                if (_first_empty_block->size >= 2 * (sizeof(size_t) + sizeof(void *))) {
-                    _first_empty_block->size -= sizeof(void *);
-                    _first_empty_descriptor = (void *)((char *)_first_empty_block + _first_empty_block->size);
-                    (*((void **)_first_empty_descriptor)) = nullptr;
-                } else {
-                    throw AllocError(AllocErrorType::NoMemory, "No Memory");
-                }
-            }
-            _descriptor = (void *)((void **)_descriptor - 1);
-        }
-        // Поиск подходящего блока свободной памяти
-        Block *tmp = nullptr;
-        Block *temp = _first_empty_block;
-        while (temp && (temp->size < N)) {
-            tmp = temp;
-            temp = temp->next;
-        }
-        if (!temp) {
-            defrag();
-            if (_first_empty_block->size < (N + sizeof(size_t))) {
+            if (!_first_empty_block) {
                 throw AllocError(AllocErrorType::NoMemory, "No Memory");
             }
-            tmp = nullptr;
-            temp = _first_empty_block;
-        }
-        if (*(size_t *)temp < (N + sizeof(Block) + sizeof(size_t))) { // Убираем блок выделенной памяти из списка.
-            if (tmp) {
-                tmp->next = temp->next;
-            } else {
-                _first_empty_block = temp->next;
+            // Ищем дескриптор для выделяемой памяти
+            if (!_first_empty_descriptor) {
+                Block *temp = _first_empty_block;
+                while (temp->next) {
+                    temp = temp->next;
+                }
+
+                if ((temp->size >= 2 * (sizeof(size_t) + sizeof(void *))) &&
+                    ((void *)((char *)temp + temp->size) == _descriptor)) {
+                    temp->size -= sizeof(void *);
+                    _first_empty_descriptor = (void *)((char *)temp + temp->size);
+                    *((void **)_first_empty_descriptor) = nullptr;
+                } else {
+                    defrag();
+                    if (_first_empty_block->size >= 2 * (sizeof(size_t) + sizeof(void *))) {
+                        _first_empty_block->size -= sizeof(void *);
+                        _first_empty_descriptor = (void *)((char *)_first_empty_block + _first_empty_block->size);
+                        (*((void **)_first_empty_descriptor)) = nullptr;
+                    } else {
+                        throw AllocError(AllocErrorType::NoMemory, "No Memory");
+                    }
+                }
+                _descriptor = (void *)((void **)_descriptor - 1);
             }
-        } else {
-            if (tmp) {
-                tmp->next = ((Block *)((char *)temp + temp->size))->next;
-                ((Block *)((char *)temp + temp->size))->size = temp->size - sizeof(size_t) - N;
-                ((Block *)((char *)temp + temp->size))->next = temp->next;
-                temp->size = N + sizeof(size_t);
-            } else {
-                _first_empty_block = (Block *)((char *)temp + sizeof(size_t) + N);
-                _first_empty_block->size = temp->size - sizeof(size_t) - N;
-                _first_empty_block->next = temp->next;
-                temp->size = N + sizeof(size_t);
+            // Поиск подходящего блока свободной памяти
+            Block *tmp = nullptr;
+            Block *temp = _first_empty_block;
+            while (temp && (temp->size < N)) {
+                tmp = temp;
+                temp = temp->next;
             }
+            if (!temp) {
+                defrag();
+                if (_first_empty_block->size < (N + sizeof(size_t))) {
+                    throw AllocError(AllocErrorType::NoMemory, "No Memory");
+                }
+                tmp = nullptr;
+                temp = _first_empty_block;
+            }
+            if (*(size_t *)temp < (N + sizeof(Block) + sizeof(size_t))) { // Убираем блок выделенной памяти из списка.
+                if (tmp) {
+                    tmp->next = temp->next;
+                } else {
+                    _first_empty_block = temp->next;
+                }
+            } else {
+                if (tmp) {
+                    tmp->next = ((Block *)((char *)temp + temp->size))->next;
+                    ((Block *)((char *)temp + temp->size))->size = temp->size - sizeof(size_t) - N;
+                    ((Block *)((char *)temp + temp->size))->next = temp->next;
+                    temp->size = N + sizeof(size_t);
+                } else {
+                    _first_empty_block = (Block *)((char *)temp + sizeof(size_t) + N);
+                    _first_empty_block->size = temp->size - sizeof(size_t) - N;
+                    _first_empty_block->next = temp->next;
+                    temp->size = N + sizeof(size_t);
+                }
+            }
+            void **temp_2;
+            Pointer result((void **)_first_empty_descriptor);
+            temp_2 = (void **)_first_empty_descriptor;
+            _first_empty_descriptor = *((void **)_first_empty_descriptor);
+            *temp_2 = (void *)((size_t *)temp + 1);
+            ++_number_descriptor;
+            return result;
         }
-        void **temp_2;
-        Pointer result((void **)_first_empty_descriptor);
-        temp_2 = (void **)_first_empty_descriptor;
-        _first_empty_descriptor = *((void **)_first_empty_descriptor);
-        *temp_2 = (void *)((size_t *)temp + 1);
-        ++_number_descriptor;
-        return result;
-    } catch (AllocError &e) {
-        std::cerr << "Allocation error type:" << e.what() << std::endl;
-        throw AllocError(AllocErrorType::NoMemory, "No Memory");
-    }
-}
 
 /**
  * TODO: semantics
  * @param p Pointer
  * @param N size_t
  */
-void Simple::realloc(Pointer &p, size_t N) {
-    if (p.get()) {
-        size_t n;
-        n = *((size_t *)p.get() - 1);
-        std::cout << "N = " << N << "; n = " << n << std::endl;
-        if (n >= N) {
-            if (N < (n + sizeof(Block))) {
-                return;
-            }
-            Block *new_block = (Block *)((char *)p.get() + N - sizeof(size_t));
-            new_block->size = n - N;
-            Block *first = _first_empty_block, *second = nullptr;
-            while (first && (first < p.get())) {
-                second = first;
-                first = first->next;
-            }
-
-            if (second) {
-                second->next = new_block;
-
-            } else {
-                _first_empty_block = new_block;
-            }
-            new_block->next = first;
-            if ((Block *)((char *)new_block + new_block->size) == new_block->next) {
-                new_block->next = first->next;
-                new_block->size += first->size;
-            }
-        } else {
-            Block *first = _first_empty_block, *second = nullptr;
-            while (first && (first < p.get())) {
-                second = first;
-                first = first->next;
-            }
-            if ((void *)((char *)p.get() + *((size_t *)p.get() - 1) - sizeof(size_t)) == (void *)first) {
-                if (first->size >= (N - n + sizeof(Block))) {
-                    Block *temp = (Block *)((char *)first + (N - n));
-                    if (second) {
-                        second->next = temp;
-                    } else {
-                        _first_empty_block = temp;
+        void Simple::realloc(Pointer &p, size_t N) {
+            if (p.get()) {
+                size_t n;
+                n = *((size_t *)p.get() - 1);
+                if (n >= N) {
+                    if (N < (n + sizeof(Block))) {
+                        return;
                     }
-                    temp->next = first->next;
-                    temp->size = first->size + n - N;
-                    *((size_t *)p.get() - 1) += N - n;
+                    Block *new_block = (Block *)((char *)p.get() + N - sizeof(size_t));
+                    new_block->size = n - N;
+                    Block *first = _first_empty_block, *second = nullptr;
+                    while (first && (first < p.get())) {
+                        second = first;
+                        first = first->next;
+                    }
+
+                    if (second) {
+                        second->next = new_block;
+
+                    } else {
+                        _first_empty_block = new_block;
+                    }
+                    new_block->next = first;
+                    if ((Block *)((char *)new_block + new_block->size) == new_block->next) {
+                        new_block->next = first->next;
+                        new_block->size += first->size;
+                    }
                 } else {
-                    if (second) {
-                        second->next = first->next;
-                    } else {
-                        _first_empty_block = first->next;
+                    Block *first = _first_empty_block, *second = nullptr;
+                    while (first && (first < p.get())) {
+                        second = first;
+                        first = first->next;
                     }
-                    *((size_t *)p.get() - 1) += first->size;
+                    if ((void *)((char *)p.get() + *((size_t *)p.get() - 1) - sizeof(size_t)) == (void *)first) {
+                        if (first->size >= (N - n + sizeof(Block))) {
+                            Block *temp = (Block *)((char *)first + (N - n));
+                            if (second) {
+                                second->next = temp;
+                            } else {
+                                _first_empty_block = temp;
+                            }
+                            temp->next = first->next;
+                            temp->size = first->size + n - N;
+                            *((size_t *)p.get() - 1) += N - n;
+                        } else {
+                            if (second) {
+                                second->next = first->next;
+                            } else {
+                                _first_empty_block = first->next;
+                            }
+                            *((size_t *)p.get() - 1) += first->size;
+                        }
+                    } else {
+                        Pointer result = alloc(N);
+                        memcpy(result.get(), p.get(), N);
+                        free(p);
+                        p = result;
+                    }
                 }
+
             } else {
-                Pointer result = alloc(N);
-                memcpy(result.get(), p.get(), N);
-                free(p);
-                p = result;
+                p = alloc(N);
             }
         }
-
-    } else {
-        p = alloc(N);
-    }
-}
 
 /**
  * TODO: semantics
  * @param p Pointer
  */
-void Simple::free(Pointer &p) {
-
-    try {
-
-        if (!p.get()) {
-            return;
-        }
-        bool flag = false;
-        void *temp = _first_empty_descriptor;
-        for (size_t i = 0; i < _number_descriptor; ++i) {
-            if (*((void **)_descriptor + i) == p.get()) {
-                flag = true;
-                while (temp) {
-                    if (temp == p.get()) {
-                        flag = false;
+        void Simple::free(Pointer &p) {
+            if (!p.get()) {
+                return;
+            }
+            bool flag = false;
+            void *temp = _first_empty_descriptor;
+            for (size_t i = 0; i < _number_descriptor; ++i) {
+                if (*((void **)_descriptor + i) == p.get()) {
+                    flag = true;
+                    while (temp) {
+                        if (temp == p.get()) {
+                            flag = false;
+                        }
+                        temp = *((void **)temp);
                     }
-                    temp = *((void **)temp);
+                }
+            }
+            if (!flag)
+                return;
+            if (p.get()) {
+                Block *first, *second = nullptr;
+                first = _first_empty_block;
+                while (first && (first < p.get())) {
+                    second = first;
+                    first = first->next;
+                }
+                if (second) {
+                    second->next = (Block *)((size_t *)p.get() - 1);
+
+                } else {
+                    _first_empty_block = (Block *)((size_t *)p.get() - 1);
+                }
+                *(void **)p.get() = first;
+                if (((char *)p.get() + *((size_t *)p.get() - 1) - sizeof(size_t)) == *(void **)p.get()) {
+                    *(Block **)p.get() = first->next;
+                    *((size_t *)p.get() - 1) += first->size;
+                }
+                if(second&&second->next == (Block*)((char*)second + second->size)){
+                    second->size += second->next->size;
+                    second->next = second->next->next;
+                }
+
+                *p._pointer = _first_empty_descriptor;
+                _first_empty_descriptor = (void *)p._pointer;
+            }
+        }
+
+/**
+ * TODO: semantics
+ */
+        void Simple::defrag() {
+
+            if (!_first_empty_block)
+                return;
+            size_t size_block;
+            Block *pointer_block;
+            Block *next_block;
+            while ((void *)((char *)_first_empty_block + _first_empty_block->size) < _descriptor) {
+                size_block = _first_empty_block->size;
+                pointer_block = _first_empty_block->next;
+                next_block = (Block *)((char *)_first_empty_block + _first_empty_block->size);
+                memcpy((void *)_first_empty_block, next_block, *((size_t *)next_block) + sizeof(size_t));
+                for (size_t i = 0; i < _number_descriptor; ++i) {
+                    if (*((void **)_descriptor + i) == (void *)((size_t *)next_block + 1)) {
+                        *((void **)_descriptor + i) = (void *)((size_t *)_first_empty_block + 1);
+                        break;
+                    }
+                }
+                _first_empty_block = (Block *)((char *)_first_empty_block + *(size_t *)_first_empty_block);
+                _first_empty_block->size = size_block;
+                _first_empty_block->next = pointer_block;
+                if ((Block *)((char *)_first_empty_block + _first_empty_block->size) == _first_empty_block->next) {
+                    _first_empty_block->size += _first_empty_block->next->size;
+                    _first_empty_block->next = _first_empty_block->next->next;
                 }
             }
         }
-        if (!flag)
-            throw AllocError(AllocErrorType::InvalidFree, "InvalidPointer");
-        if (p.get()) {
-            Block *first, *second = nullptr;
-            first = _first_empty_block;
-            while (first && (first < p.get())) {
-                second = first;
-                first = first->next;
-            }
-            if (second) {
-                second->next = (Block *)((size_t *)p.get() - 1);
-
-            } else {
-                _first_empty_block = (Block *)((size_t *)p.get() - 1);
-            }
-            *(void **)p.get() = first;
-            if (((char *)p.get() + *((size_t *)p.get() - 1) - sizeof(size_t)) == *(void **)p.get()) {
-                *(Block **)p.get() = first->next;
-                *((size_t *)p.get() - 1) += first->size;
-            }
-
-            *p._pointer = _first_empty_descriptor;
-            _first_empty_descriptor = (void *)p._pointer;
-        }
-    } catch (AllocError &e) {
-        std::cerr << "Allocation error type:" << e.what() << std::endl;
-    }
-}
 
 /**
  * TODO: semantics
  */
-void Simple::defrag() {
+        std::string Simple::dump() const { return ""; }
 
-    if (!_first_empty_block)
-        return;
-    size_t size_block;
-    Block *pointer_block;
-    Block *next_block;
-    while ((void *)((char *)_first_empty_block + _first_empty_block->size) < _descriptor) {
-        size_block = _first_empty_block->size;
-        pointer_block = _first_empty_block->next;
-        next_block = (Block *)((char *)_first_empty_block + _first_empty_block->size);
-        memcpy((void *)_first_empty_block, next_block, *((size_t *)next_block) + sizeof(size_t));
-        for (size_t i = 0; i < _number_descriptor; ++i) {
-            if (*((void **)_descriptor + i) == (void *)((size_t *)next_block + 1)) {
-                *((void **)_descriptor + i) = (void *)((size_t *)_first_empty_block + 1);
-                break;
-            }
-        }
-        _first_empty_block = (Block *)((char *)_first_empty_block + *(size_t *)_first_empty_block);
-        _first_empty_block->size = size_block;
-        _first_empty_block->next = pointer_block;
-        if ((Block *)((char *)_first_empty_block + _first_empty_block->size) == _first_empty_block->next) {
-            _first_empty_block->size += _first_empty_block->next->size;
-            _first_empty_block->next = _first_empty_block->next->next;
-        }
-    }
-}
-
-/**
- * TODO: semantics
- */
-std::string Simple::dump() const { return ""; }
-
-} // namespace Allocator
+    } // namespace Allocator
 } // namespace Afina
